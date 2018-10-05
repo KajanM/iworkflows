cde84648c205ab6ae1ac4b1412518b3e9e6dd225
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-05 20:14:44 +0530

    initialized the project

---
+++ src/main/java/com/kajan/iworkflows/IworkflowsApplication.java
@@ -0,0 +1,12 @@
+package com.kajan.iworkflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IworkflowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IworkflowsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kajan/iworkflows/IworkflowsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kajan.iworkflows;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IworkflowsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
