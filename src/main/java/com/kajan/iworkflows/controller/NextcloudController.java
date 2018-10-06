90ccbde434a161e1e6807b2b1f2bd3512c19e4fe
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-06 09:36:27 +0530

    extract nextcloud header implementation to separate method

--- src/main/java/com/kajan/iworkflows/controller/NextcloudController.java
+++ src/main/java/com/kajan/iworkflows/controller/NextcloudController.java
@@ -27,6 +27,15 @@ public class NextcloudController {
     @GetMapping("/nextcloud-user")
     @ResponseBody
     public String getNextcloudUserInfo(Principal principal) {
+        // TODO: get user id from LDAP
+        String userInfoUri = "http://localhost:8090/nextcloud/ocs/v1.php/cloud/users/kajan";
+
+        HttpEntity<String> httpEntity = new HttpEntity<>("headers", getNextcloudHeaders(principal));
+        ResponseEntity<String> responseEntity = restTemplate.exchange(userInfoUri, HttpMethod.GET, httpEntity, String.class);
+        return responseEntity.toString();
+    }
+
+    private HttpHeaders getNextcloudHeaders(Principal principal) {
         AccessToken accessToken = oauthTokenService.getAccessToken(principal);
         logger.debug("AccessToken: " + accessToken);
 
@@ -34,12 +43,6 @@ public class NextcloudController {
         headers.add("OCS-APIRequest", "true");
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getValue());
-
-        // TODO: get user id from LDAP
-        String userInfoUri = "http://localhost:8090/nextcloud/ocs/v1.php/cloud/users/kajan";
-
-        HttpEntity<String> httpEntity = new HttpEntity<>("headers", headers);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(userInfoUri, HttpMethod.GET, httpEntity, String.class);
-        return responseEntity.toString();
+        return headers;
     }
 }
