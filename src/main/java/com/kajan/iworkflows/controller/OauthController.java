8600d956fbaa7ff84fdfb1fbd5ecb8bb2b1df885
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-06 12:34:04 +0530

    enable automated oauth2 client integration

--- src/main/java/com/kajan/iworkflows/controller/OauthController.java
+++ src/main/java/com/kajan/iworkflows/controller/OauthController.java
@@ -5,15 +5,21 @@ import com.kajan.iworkflows.util.Constants.OauthRegistrationId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class OauthController {
@@ -23,26 +29,44 @@ public class OauthController {
     @Autowired
     private OauthControllerService oauthControllerService;
 
+    private final String authorizationRequestBaseUri = "authorize/oauth2";
+    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
     @GetMapping("/authorize")
-    public String getAuthorizationPage() {
+    public String getAuthorizationPage(Model model) {
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+                .as(Iterable.class);
+        if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+
+        clientRegistrations.forEach(registration -> oauth2AuthenticationUrls.put(registration.getClientName(), authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+
         return "authorize";
     }
 
-    @RequestMapping("/redirect/nextcloud")
-    public ModelAndView redirectToNextCloudForAuthorization() {
+    @RequestMapping("/authorize/oauth2/{registrationId}")
+    public ModelAndView redirectToNextCloudForAuthorization(@PathVariable String registrationId) {
 
-        logger.debug("hit /redirect/nextcloud end-point");
+        logger.debug("hit /authorize/oauth2/" + registrationId + " end-point");
 
-        URI requestURI = oauthControllerService.getAuthorizationCodeRequestUri(OauthRegistrationId.NEXTCLOUD);
+        OauthRegistrationId oauthRegistrationId = OauthRegistrationId.valueOf(registrationId.toUpperCase());
+
+        URI requestURI = oauthControllerService.getAuthorizationCodeRequestUri(oauthRegistrationId);
         return new ModelAndView("redirect:" + requestURI.toASCIIString());
     }
 
-    @RequestMapping("/login/oauth2/code/nextcloud")
-    public String getNextcloudAccessToken(HttpServletRequest httpServletRequest, Principal principal, Model model) {
+    @RequestMapping("/login/oauth2/code/{registrationId}")
+    public String getNextcloudAccessToken(@PathVariable String registrationId, HttpServletRequest httpServletRequest, Principal principal, Model model) {
 
-        logger.debug("hit /login/oauth2/code/nextcloud end-point");
+        logger.debug("hit /login/oauth2/code/" + registrationId + " end-point");
 
-        oauthControllerService.exchangeAuthorizationCodeForAccessToken(OauthRegistrationId.NEXTCLOUD, httpServletRequest, principal);
+        OauthRegistrationId oauthRegistrationId = OauthRegistrationId.valueOf(registrationId.toUpperCase());
+        oauthControllerService.exchangeAuthorizationCodeForAccessToken(oauthRegistrationId, httpServletRequest, principal);
         model.addAttribute("accessToken", oauthControllerService.getAccessToken(principal));
         model.addAttribute("refreshToken", oauthControllerService.getRefreshToken(principal));
         return "authorization-success";
