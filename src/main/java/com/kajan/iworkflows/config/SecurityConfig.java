55d5c61491cbfe7aa35b9fbbe731b43a3017f604
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-07 22:04:48 +0530

    ldap integration

--- src/main/java/com/kajan/iworkflows/config/SecurityConfig.java
+++ src/main/java/com/kajan/iworkflows/config/SecurityConfig.java
@@ -1,12 +1,11 @@
 package com.kajan.iworkflows.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,22 +31,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     // @formatter:on
 
+    //@Override
+    //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //    auth.inMemoryAuthentication()
+    //            .passwordEncoder(passwordEncoder())
+    //            .withUser("kajan")
+    //            .password(passwordEncoder().encode("kajan"))
+    //            .roles("USER")
+    //            .and()
+    //            .withUser("admin")
+    //            .password(passwordEncoder().encode("admin"))
+    //            .roles("ADMIN");
+    //}
+
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("kajan")
-                .password(passwordEncoder().encode("kajan"))
-                .roles("USER")
+    @Profile("ldap")
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
                 .and()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN");
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    //@Bean
+    //public PasswordEncoder passwordEncoder() {
+    //    return new BCryptPasswordEncoder();
+    //}
 
 }
