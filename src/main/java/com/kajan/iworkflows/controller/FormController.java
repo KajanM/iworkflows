374c37750fd7aaa72f8f37590910554903e31c61
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-10-15 14:14:37 +0530

    added controller level requestmapping for forms

--- src/main/java/com/kajan/iworkflows/controller/FormController.java
+++ src/main/java/com/kajan/iworkflows/controller/FormController.java
@@ -3,12 +3,14 @@ package com.kajan.iworkflows.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
 @Controller
+@RequestMapping("/forms")
 public class FormController {
-    @GetMapping({"/forms/leave-form", "/forms/leave-form.html"})
+    @GetMapping({"leave-form", "leave-form.html"})
     public String leaveForm(Principal principal, Model model) {
         return "leave-form";
     }
