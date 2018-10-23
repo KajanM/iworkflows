7f56593ac2df0dc0938607d722113c848a274226
kasthuri <kasthuri.14@cse.mrt.ac.lk>
2018-10-23 07:03:37 +0530

    gracefully handle exceptions

--- src/main/java/com/kajan/iworkflows/controller/TestController.java
+++ src/main/java/com/kajan/iworkflows/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.kajan.iworkflows.controller;
 
+import com.kajan.iworkflows.exception.UnauthorizedException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,4 +14,17 @@ public class TestController {
     public String getTestPage() {
         return "test";
     }
+
+    @GetMapping("/500")
+    public String simulate500() {
+        String a = "panda";
+        a.charAt(50);
+        return null;
+    }
+
+    @GetMapping("/401")
+    public String simulate401() {
+        throw new UnauthorizedException();
+    }
+
 }
