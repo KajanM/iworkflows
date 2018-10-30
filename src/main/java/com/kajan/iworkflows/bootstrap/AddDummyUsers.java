f414fa91e9322f55f9ecbcc108915ba8c314e6c0
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-30 06:28:41 +0530

    support JWT authentication over REST

--- src/main/java/com/kajan/iworkflows/bootstrap/AddDummyUsers.java
+++ src/main/java/com/kajan/iworkflows/bootstrap/AddDummyUsers.java
@@ -1,17 +1,29 @@
 package com.kajan.iworkflows.bootstrap;
 
+import com.kajan.iworkflows.model.ApplicationUser;
 import com.kajan.iworkflows.model.mock.DummyUserStore;
+import com.kajan.iworkflows.repository.ApplicationUserRepository;
 import com.kajan.iworkflows.repository.DummyUserStoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
 
 @Component
 public class AddDummyUsers implements ApplicationListener<ContextRefreshedEvent> {
 
-    @Autowired
     DummyUserStoreRepository userStoreRepository;
+    ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    public AddDummyUsers(DummyUserStoreRepository userStoreRepository,
+                         ApplicationUserRepository applicationUserRepository) {
+        this.userStoreRepository = userStoreRepository;
+        this.applicationUserRepository = applicationUserRepository;
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -19,9 +31,19 @@ public class AddDummyUsers implements ApplicationListener<ContextRefreshedEvent>
     }
 
     private void initUserData() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         userStoreRepository.save(new DummyUserStore("Kasthuri", "140379F", "Engineering", "CSE", "Student",
                 "kasthuri.14@cse.mrt.ac.lk", "077-9577217", 3, 4, 1));
         userStoreRepository.save(new DummyUserStore("kajan", "140709U", "Engineering", "CSE", "Student",
-                "kajan.14@cse.mrt.ac.lk", "077-0774946", 2,0,6));
+                "kajan.14@cse.mrt.ac.lk", "077-0774946", 2, 0, 6));
+
+        Stream.of("kasthuri.14@cse.mrt.ac.lk", "kajan.14@cse.mrt.ac.lk", "kirisanth.14@cse.mrt.ac.lk", "ramiya.14@cse.mrt.ac.lk").forEach(name -> {
+            ApplicationUser user = new ApplicationUser();
+            user.setUsername(name);
+            user.setPassword(bCryptPasswordEncoder.encode("password"));
+            applicationUserRepository.save(user);
+        });
+        applicationUserRepository.findAll().forEach(System.out::println);
     }
+
 }
--- src/main/java/com/kajan/iworkflows/config/SecurityConfig.java
+++ src/main/java/com/kajan/iworkflows/config/SecurityConfig.java
@@ -1,57 +1,66 @@
 package com.kajan.iworkflows.config;
 
+import com.kajan.iworkflows.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static constants.SecurityConstants.SIGN_UP_URL;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // @formatter:off
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers(
-                            "/js/**",
-                            "/css/**",
-                            "/img/**",
-                            "/webjars/**", "/h2/**", "/dist/**", "/vendor/**", "/favicon.ico").permitAll()
-                    .antMatchers("/user/**").hasRole("USER")
-                    .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .csrf().disable()
-                .headers()
-                .frameOptions().disable()
-                .and()
-                .logout().logoutUrl("/logout");
-    }
-    // @formatter:on
+    //// @formatter:off
+    //@Override
+    //protected void configure(HttpSecurity http) throws Exception {
+    //    http
+    //            .authorizeRequests()
+    //                .antMatchers(
+    //                        "/js/**",
+    //                        "/css/**",
+    //                        "/img/**",
+    //                        "/webjars/**", "/h2/**", "/dist/**", "/vendor/**", "/favicon.ico").permitAll()
+    //                .antMatchers("/user/**").hasRole("USER")
+    //                .anyRequest().authenticated()
+    //            .and()
+    //            .formLogin().permitAll()
+    //            .and()
+    //            .csrf().disable()
+    //            .headers()
+    //            .frameOptions().disable()
+    //            .and()
+    //            .logout().logoutUrl("/logout");
+    //}
+    //// @formatter:on
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("kajan")
-                .password(passwordEncoder().encode("kajan"))
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .and()
-                .withUser("Kasthuri")
-                .password(passwordEncoder().encode("kachu123"))
-                .roles("USER");
-
-
-    }
+    //@Override
+    //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //    auth.inMemoryAuthentication()
+    //            .passwordEncoder(passwordEncoder())
+    //            .withUser("kajan")
+    //            .password(passwordEncoder().encode("kajan"))
+    //            .roles("USER")
+    //            .and()
+    //            .withUser("admin")
+    //            .password(passwordEncoder().encode("admin"))
+    //            .roles("ADMIN")
+    //            .and()
+    //            .withUser("Kasthuri")
+    //            .password(passwordEncoder().encode("kachu123"))
+    //            .roles("USER");
+    //
+    //
+    //}
 
     //@Override
     //@Profile("ldap")
@@ -68,9 +77,63 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //            .passwordAttribute("userPassword");
     //}
 
+    //@Override
+    //protected void configure(AuthenticationManagerBuilder auth)
+    //        throws Exception {
+    //    auth
+    //            .inMemoryAuthentication()
+    //            .withUser("user")
+    //            .password(passwordEncoder().encode("password"))
+    //            .roles("USER");
+    //}
+    //
+    //@Override
+    //protected void configure(HttpSecurity http)
+    //        throws Exception {
+    //    http.csrf().disable()
+    //            .authorizeRequests()
+    //            .antMatchers("/login").permitAll()
+    //            .anyRequest()
+    //            .authenticated()
+    //            .and()
+    //            .httpBasic();
+    //}
+    //
+    //@Bean
+    //public PasswordEncoder passwordEncoder() {
+    //    return new BCryptPasswordEncoder();
+    //}
+
+    private UserDetailsServiceImpl userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 
 }
