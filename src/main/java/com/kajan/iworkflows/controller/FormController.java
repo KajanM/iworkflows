7b63110b2a0816d0e58d53961c682e12cb327d71
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-11-02 00:34:16 +0530

    add form controller to retrieve leave form data

--- src/main/java/com/kajan/iworkflows/controller/FormController.java
+++ src/main/java/com/kajan/iworkflows/controller/FormController.java
@@ -1,39 +1,48 @@
 package com.kajan.iworkflows.controller;
 
+import com.kajan.iworkflows.model.LeaveFormData;
 import com.kajan.iworkflows.model.mock.DummyUserStore;
 import com.kajan.iworkflows.service.impl.DummyUserStoreServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/forms")
 public class FormController {
 
-    @Autowired
-    private DummyUserStoreServiceImpl storeService;
+    private final DummyUserStoreServiceImpl storeService;
 
-    @GetMapping({"leave-form", "leave-form.html"})
-    public String leaveForm(Principal principal, Model model) {
+    @Autowired
+    public FormController(DummyUserStoreServiceImpl storeService) {
+        this.storeService = storeService;
+    }
+
+    @GetMapping("leave-form")
+    public LeaveFormData leaveForm(Principal principal) {
         List<DummyUserStore> userStoreList = new ArrayList<>();
 
         storeService.findByPrincipal(principal.getName()).forEach(userStoreList::add);
-        model.addAttribute("employeeId", userStoreList.get(0).getEmployeeId());
-        model.addAttribute("principal", userStoreList.get(0).getPrincipal());
-        model.addAttribute("faculty", userStoreList.get(0).getFaculty());
-        model.addAttribute("department", userStoreList.get(0).getDepartment());
-        model.addAttribute("role", userStoreList.get(0).getRole());
-        model.addAttribute("email", userStoreList.get(0).getEmail());
-        model.addAttribute("mobileNo", userStoreList.get(0).getMobileNo());
-        model.addAttribute("casual", userStoreList.get(0).getCasual());
-        model.addAttribute("medical", userStoreList.get(0).getMedical());
-        model.addAttribute("vacation", userStoreList.get(0).getVacation());
-        return "leave-form";
+        DummyUserStore userStore = userStoreList.get(0);
+        LeaveFormData form = new LeaveFormData();
+
+        form.setId(userStore.getId());
+        form.setPrincipal(userStore.getPrincipal());
+        form.setEmployeeId(userStore.getEmployeeId());
+        form.setFaculty(userStore.getFaculty());
+        form.setDepartment(userStore.getDepartment());
+        form.setRole(userStore.getRole());
+        form.setEmail(userStore.getEmail());
+        form.setMobileNo(userStore.getMobileNo());
+        form.setCasual(userStore.getCasual());
+        form.setMedical(userStore.getMedical());
+        form.setVacation(userStore.getVacation());
+
+        return form;
     }
 }
