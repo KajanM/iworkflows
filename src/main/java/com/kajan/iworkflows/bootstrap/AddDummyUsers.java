e83e0b3c780151b3b957aed9d2c0c51098772ea6
KajanM <kajan.14@cse.mrt.ac.lk>
2018-11-02 00:27:19 +0530

    use shorthand method to initialize database

--- src/main/java/com/kajan/iworkflows/bootstrap/AddDummyUsers.java
+++ src/main/java/com/kajan/iworkflows/bootstrap/AddDummyUsers.java
@@ -2,33 +2,26 @@ package com.kajan.iworkflows.bootstrap;
 
 import com.kajan.iworkflows.model.mock.DummyUserStore;
 import com.kajan.iworkflows.repository.DummyUserStoreRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
-public class AddDummyUsers implements ApplicationListener<ContextRefreshedEvent> {
+@Configuration
+@Slf4j
+public class AddDummyUsers {
 
-    DummyUserStoreRepository userStoreRepository;
-
-    @Autowired
-    public AddDummyUsers(DummyUserStoreRepository userStoreRepository) {
-        this.userStoreRepository = userStoreRepository;
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        initUserData();
-    }
-
-    private void initUserData() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        userStoreRepository.save(new DummyUserStore("Kasthuri", "140379F", "Engineering", "CSE", "Student",
-                "kasthuri.14@cse.mrt.ac.lk", "077-9577217", 3, 4, 1));
-        userStoreRepository.save(new DummyUserStore("kajan", "140709U", "Engineering", "CSE", "Student",
-                "kajan.14@cse.mrt.ac.lk", "077-0774946", 2, 0, 6));
+    @Bean
+    CommandLineRunner initDatabase(DummyUserStoreRepository userStoreRepository) {
+        return args -> {
+            log.debug("Preloading " + userStoreRepository.save(new DummyUserStore("Kasthuri", "140379F", "Engineering", "CSE", "Student",
+                    "kasthuri.14@cse.mrt.ac.lk", "077-9577217", 3, 4, 1)));
+            log.debug("Preloading" + userStoreRepository.save(new DummyUserStore("kajan", "140709U", "Engineering", "CSE", "Student",
+                    "kajan.14@cse.mrt.ac.lk", "077-0774946", 2, 0, 6)));
+        };
     }
 
 }
