d4dac9c78761c54e74518da652b919bd57c88c48
kajan <kajan.14@cse.mrt.ac.lk>
2018-11-11 08:09:36 +0530

    organize imports in SecurityConfig

--- src/main/java/com/kajan/iworkflows/config/SecurityConfig.java
+++ src/main/java/com/kajan/iworkflows/config/SecurityConfig.java
@@ -3,12 +3,12 @@ package com.kajan.iworkflows.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
