1c79eb336e102b8bc3a9c9529a5d54e51333f375
kajan <kajan.14@cse.mrt.ac.lk>
2018-11-13 12:17:07 +0530

    change leave-process start url

--- src/main/java/com/kajan/iworkflows/util/WorkflowConstants.java
+++ src/main/java/com/kajan/iworkflows/util/WorkflowConstants.java
@@ -5,6 +5,7 @@ public class WorkflowConstants {
 
     // leave process constants
     public static final String LEAVE_DETAILS_KEY = "leave_details";
+    public static final String LEAVE_PROCESS_DEFINITION_KEY = "leave_process";
     public static final String DUE_DATE_KEY = "due_date";
     public static final String RECOMMENDATION_KEY = "recommendation";
 
