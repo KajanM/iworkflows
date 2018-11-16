44c05ed5e55dd449ff942f0643e8090d62100f4f
kajan <kajan.14@cse.mrt.ac.lk>
2018-11-16 17:18:36 +0530

    include task status when getting submitted request details

--- src/main/java/com/kajan/iworkflows/workflow/dto/SubmittedRequestBasicDetails.java
+++ src/main/java/com/kajan/iworkflows/workflow/dto/SubmittedRequestBasicDetails.java
@@ -2,6 +2,7 @@ package com.kajan.iworkflows.workflow.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.camunda.bpm.engine.history.HistoricTaskInstance;
 import org.camunda.bpm.engine.task.Task;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class SubmittedRequestBasicDetails implements Serializable {
     private String processInstanceId;
     private String processDefinitionId;
     private String name;
+    private String status;
     private Date submittedDate;
     private Date dueDate;
     private String assignee;
@@ -24,10 +26,22 @@ public class SubmittedRequestBasicDetails implements Serializable {
         request.setTaskId(task.getId());
         request.setProcessInstanceId(task.getProcessInstanceId());
         request.setProcessDefinitionId(task.getProcessDefinitionId());
+        request.setStatus("in_progress");
         request.setName(task.getName());
         request.setSubmittedDate(task.getCreateTime());
         request.setDueDate(task.getDueDate());
         request.setAssignee(task.getAssignee());
         return request;
+    }
+
+    public static SubmittedRequestBasicDetails fromHistoricTaskInstance(HistoricTaskInstance instance) {
+        SubmittedRequestBasicDetails details = new SubmittedRequestBasicDetails();
+        details.setTaskId(instance.getId());
+        details.setProcessInstanceId(instance.getProcessInstanceId());
+        details.setProcessDefinitionId(instance.getProcessDefinitionId());
+        details.setName(instance.getName());
+        details.setSubmittedDate(instance.getStartTime());
+        details.setDueDate(instance.getDueDate());
+        return details;
     }
 }
