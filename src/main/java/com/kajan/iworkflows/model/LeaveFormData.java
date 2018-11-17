24a84b9a06b8ef801fd5f7f3e9d6c268e0cbce50
RamiyaRatnarajah <prramiya9481@gmail.com>
2018-11-17 20:07:03 +0530

    auto assign tasks based on learnorg

--- src/main/java/com/kajan/iworkflows/model/LeaveFormData.java
+++ src/main/java/com/kajan/iworkflows/model/LeaveFormData.java
@@ -14,7 +14,33 @@ public class LeaveFormData {
     private String role;
     private String email;
     private String mobileNo;
+    private String telephoneNo;
+    private String address;
     private int casual;
     private int medical;
     private int vacation;
+    private int casualAllowed;
+    private int medicalAllowed;
+    private int vacationAllowed;
+
+    public static LeaveFormData fromUserStore(UserStore userStore) {
+        LeaveFormData data = new LeaveFormData();
+//        data.setId(userStore.getId());
+        data.setPrincipal(userStore.getFirstName());
+        data.setEmployeeId(userStore.getUserID());
+        data.setFaculty(userStore.getFaculty());
+        data.setDepartment(userStore.getDepartment());
+        data.setRole(userStore.getRole());
+        data.setEmail(userStore.getPrimaryEmail());
+        data.setMobileNo(userStore.getPermanatTelephone());
+        data.setTelephoneNo(userStore.getCurrentHomeTelephone());
+        data.setAddress(userStore.getCurrentAddress());
+        data.setCasual(userStore.getCasual());
+        data.setMedical(userStore.getMedical());
+        data.setVacation(userStore.getVacation());
+        data.setCasualAllowed(userStore.getCasualAllowed());
+        data.setMedicalAllowed(userStore.getMedicalAllowed());
+        data.setVacationAllowed(userStore.getVacationAllowed());
+        return data;
+    }
 }
--- src/main/java/com/kajan/iworkflows/workflow/CamundaFormController.java
+++ src/main/java/com/kajan/iworkflows/workflow/CamundaFormController.java
@@ -1,8 +1,10 @@
 package com.kajan.iworkflows.workflow;
 
 import com.kajan.iworkflows.model.LeaveFormData;
+import com.kajan.iworkflows.model.UserStore;
 import com.kajan.iworkflows.model.mock.DummyUserStore;
 import com.kajan.iworkflows.service.impl.DummyUserStoreServiceImpl;
+import com.kajan.iworkflows.service.impl.LearnOrgServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,31 +19,24 @@ import java.util.List;
 public class CamundaFormController {
 
     private final DummyUserStoreServiceImpl storeService;
+    private final LearnOrgServiceImpl learnOrgService;
 
     @Autowired
-    public CamundaFormController(DummyUserStoreServiceImpl storeService) {
+    public CamundaFormController(DummyUserStoreServiceImpl storeService, LearnOrgServiceImpl learnOrgService) {
         this.storeService = storeService;
+        this.learnOrgService = learnOrgService;
     }
 
     @GetMapping("leave-form")
     public LeaveFormData leaveForm(Principal principal) {
-        List<DummyUserStore> userStoreList = new ArrayList<>();
+//        List<DummyUserStore> userStoreList = new ArrayList<>();
+//
+//        storeService.findByPrincipal(principal.getName()).forEach(userStoreList::add);
+//        DummyUserStore userStore = userStoreList.get(0);
+//        LeaveFormData form = new LeaveFormData();
 
-        storeService.findByPrincipal(principal.getName()).forEach(userStoreList::add);
-        DummyUserStore userStore = userStoreList.get(0);
-        LeaveFormData form = new LeaveFormData();
-
-        form.setId(userStore.getId());
-        form.setPrincipal(userStore.getPrincipal());
-        form.setEmployeeId(userStore.getEmployeeId());
-        form.setFaculty(userStore.getFaculty());
-        form.setDepartment(userStore.getDepartment());
-        form.setRole(userStore.getRole());
-        form.setEmail(userStore.getEmail());
-        form.setMobileNo(userStore.getMobileNo());
-        form.setCasual(userStore.getCasual());
-        form.setMedical(userStore.getMedical());
-        form.setVacation(userStore.getVacation());
+        UserStore userStore = learnOrgService.getLearnOrgUserInfo(principal);
+        LeaveFormData form = LeaveFormData.fromUserStore(userStore);
 
         return form;
     }
