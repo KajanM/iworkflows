7efea4e2e2cf162528fe822a3e00a454d1b5f129
KajanM <kajan.14@cse.mrt.ac.lk>
2018-11-17 19:22:33 +0530

    general fixes (#39)
    * remove unnecessary nextcloud controller
    
    * fix wrong name

--- src/main/java/com/kajan/iworkflows/service/impl/ServerSideEventServiceImpl.java
+++ src/main/java/com/kajan/iworkflows/service/impl/ServerSideEventServiceImpl.java
@@ -1,6 +1,6 @@
 package com.kajan.iworkflows.service.impl;
 
-import com.kajan.iworkflows.controller.ServerSideEventController;
+import com.kajan.iworkflows.controller.ServerSentEventController;
 import com.kajan.iworkflows.service.ServerSideEventService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -15,10 +15,10 @@ public class ServerSideEventServiceImpl implements ServerSideEventService {
     @Override
     public <T> void emitServerSideEvents(Principal principal, T data) {
         try {
-            ServerSideEventController.emitters.get(principal).send(data, MediaType.APPLICATION_JSON);
+            ServerSentEventController.emitters.get(principal).send(data, MediaType.APPLICATION_JSON);
         } catch (IOException e) {
             log.error("Unable to emit event to client. Principal: {]", principal, e);
-            ServerSideEventController.emitters.remove(principal);
+            ServerSentEventController.emitters.remove(principal);
         }
     }
 }
