9fe68b7510ec781196ce9ed1a913659ec649f7d3
RamiyaRatnarajah <prramiya9481@gmail.com>
2018-11-22 19:48:08 +0530

    remove static contents

--- src/main/resources/static/assets/angular-material-examples/select-no-ripple/select-no-ripple-example.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-/** @title Select with no option ripple */
-@Component({
-  selector: 'select-no-ripple-example',
-  templateUrl: 'select-no-ripple-example.html',
-  styleUrls: ['select-no-ripple-example.css'],
-})
-export class SelectNoRippleExample {}
