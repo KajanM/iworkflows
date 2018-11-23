8cb0c4eec4ced9f1bff25faec6e23c9ce3d02e38
RamiyaRatnarajah <prramiya9481@gmail.com>
2018-11-23 11:38:32 +0530

    remove static

--- src/main/resources/static/assets/angular-material-examples/form-field-overview/form-field-overview-example.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-/** @title Simple form field */
-@Component({
-  selector: 'form-field-overview-example',
-  templateUrl: 'form-field-overview-example.html',
-  styleUrls: ['form-field-overview-example.css'],
-})
-export class FormFieldOverviewExample {}
