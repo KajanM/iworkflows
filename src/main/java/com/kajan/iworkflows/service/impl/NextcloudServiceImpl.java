2014b72ed326757ad681a5a9c75941e0f3fe91cd
KajanM <kajan.14@cse.mrt.ac.lk>
2018-11-24 03:14:28 +0530

    share nextcloud files/folders over REST (#61)
    * remove unnecessary todo
    
    * remove unused method
    
    * share a nextcloud file/folder over REST

--- src/main/java/com/kajan/iworkflows/service/impl/NextcloudServiceImpl.java
+++ src/main/java/com/kajan/iworkflows/service/impl/NextcloudServiceImpl.java
@@ -13,9 +13,13 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.NotImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
@@ -39,6 +43,9 @@ public class NextcloudServiceImpl implements NextcloudService {
     @Value("${nextcloud.uri.file}")
     private String FILE_ROOT_URI_TEMPLATE;
 
+    @Value("${nextcloud.uri.ocs-share-api}")
+    private String OCS_SHARE_ROOT_URI;
+
     @Value("${iworkflows.credentials.nextcloud.username}")
     private String IWORKFLOWS_USERNAME;
 
@@ -47,10 +54,11 @@ public class NextcloudServiceImpl implements NextcloudService {
 
     private OauthTokenService oauthTokenService;
     private Sardine webDavClient;
+    private RestTemplate restTemplate;
 
     @Override
     public InputStream getFile(String principal, String filePath) {
-        String uri = getUri(principal, filePath);
+        String uri = getResourceUri(principal, filePath);
 
         log.info("Making request to {} to download the file", uri);
         try {
@@ -66,7 +74,7 @@ public class NextcloudServiceImpl implements NextcloudService {
     @Override
     public ResponseEntity<String> uploadFile(String principal, String filePath, InputStream fileContent) {
 
-        String uri = getUri(principal, filePath);
+        String uri = getResourceUri(principal, filePath);
         log.info("Attempting to upload file to {}", uri);
 
         try {
@@ -88,7 +96,7 @@ public class NextcloudServiceImpl implements NextcloudService {
 
         List<DavResource> resources;
         try {
-            resources = sardine.list(getUri(principal, filePath));
+            resources = sardine.list(getResourceUri(principal, filePath));
         } catch (IOException e) {
             log.error("Unable to retrieve directory list from NextCloud", e);
             throw new IworkflowsWebDavException("Unable to retrieve directory list from NextCloud");
@@ -108,7 +116,7 @@ public class NextcloudServiceImpl implements NextcloudService {
             throw new NotImplementedException();
         }
         Sardine sardine = SardineFactory.begin(IWORKFLOWS_USERNAME, IWORKFLOWS_PASSWORD);
-        String uri = getUri(principal, directoryPath);
+        String uri = getResourceUri(principal, directoryPath);
         try {
             log.info("Attempting to create directory in {}", uri);
             sardine.createDirectory(uri);
@@ -126,14 +134,38 @@ public class NextcloudServiceImpl implements NextcloudService {
         }
         Sardine sardine = SardineFactory.begin(IWORKFLOWS_USERNAME, IWORKFLOWS_PASSWORD);
         try {
-            return sardine.exists(getUri(principal, resourcePath));
+            return sardine.exists(getResourceUri(principal, resourcePath));
         } catch (IOException e) {
             log.error("Unable to check if the resource exist in NextCloud or not", e);
             throw new IworkflowsWebDavException("Unable to check if the resource exist in NextCloud or not");
         }
     }
 
-    private String getUri(String principal, String filePath) {
+    @Override
+    public ResponseEntity<String> share(String principal, String resourcePath) {
+        log.debug("Attempting to share {} to {}", resourcePath, principal);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("OCS-APIRequest", "true");
+        headers.add("Authorization", getIworkflowsAuthorizationHeaderValue());
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(OCS_SHARE_ROOT_URI);
+        builder.queryParam("format", "json");
+        builder.queryParam("path", resourcePath);
+        builder.queryParam("shareType", 0);
+        builder.queryParam("shareWith", principal);
+        builder.queryParam("permissions", 1);
+
+        HttpEntity entity = new HttpEntity("", headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                builder.toUriString(),
+                HttpMethod.POST,
+                entity,
+                String.class);
+        log.debug("Successfully shared {} with {}", resourcePath, principal);
+        return response;
+    }
+
+    private String getResourceUri(String principal, String filePath) {
         return FILE_ROOT_URI_TEMPLATE.replace(PLACEHOLDER_USERID, UriUtils.encodePathSegment(principal, "UTF-8"))
                 .replace(PLACEHOLDER_FILE_PATH, filePath);
     }
@@ -143,10 +175,7 @@ public class NextcloudServiceImpl implements NextcloudService {
 
         // create basic auth header for iworkflows system user
         if (principal.equals(IWORKFLOWS_USERNAME)) {
-            String auth = IWORKFLOWS_USERNAME + ":" + IWORKFLOWS_PASSWORD;
-            byte[] encodedAuth = Base64.encodeBase64(
-                    auth.getBytes(Charset.forName("US-ASCII")));
-            headers.put(HttpHeaders.AUTHORIZATION, HEADER_VALUE_BASIC + new String(encodedAuth));
+            headers.put(HttpHeaders.AUTHORIZATION, getIworkflowsAuthorizationHeaderValue());
             return headers;
         }
 
@@ -159,6 +188,13 @@ public class NextcloudServiceImpl implements NextcloudService {
         return headers;
     }
 
+    private String getIworkflowsAuthorizationHeaderValue() {
+        String auth = IWORKFLOWS_USERNAME + ":" + IWORKFLOWS_PASSWORD;
+        byte[] encodedAuth = Base64.encodeBase64(
+                auth.getBytes(Charset.forName("US-ASCII")));
+        return HEADER_VALUE_BASIC + new String(encodedAuth);
+    }
+
     @Autowired
     public void setOauthTokenService(OauthTokenService oauthTokenService) {
         this.oauthTokenService = oauthTokenService;
@@ -167,5 +203,10 @@ public class NextcloudServiceImpl implements NextcloudService {
     @Autowired
     public void setWebDavClient(Sardine webDavClient) {
         this.webDavClient = webDavClient;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 }
