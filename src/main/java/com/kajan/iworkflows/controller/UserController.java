e5094004974128da65160855d148f21846a2af9a
kajan <kajan.14@cse.mrt.ac.lk>
2018-12-04 05:58:05 +0530

    remove method level @CrossOrigin annotation

--- src/main/java/com/kajan/iworkflows/controller/UserController.java
+++ src/main/java/com/kajan/iworkflows/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.kajan.iworkflows.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +8,6 @@ import java.security.Principal;
 @RestController
 public class UserController {
 
-    @CrossOrigin
     @GetMapping("api/v1/user")
     public Principal user(Principal user) {
         return user;
